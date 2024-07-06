5f1f5a50b45589d2183390107de61f704aaa1b1e
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2024-07-06 19:29:10 +0300

    update DB name

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,16 +1,16 @@
 const DButils = require("./DButils");
 
 async function markAsFavorite(user_id, recipe_id){
-    await DButils.execQuery(`insert into FavoriteRecipes values ('${user_id}',${recipe_id})`);
+    await DButils.execQuery(`insert into favoriterecipes values ('${user_id}',${recipe_id})`);
 }
 
 async function getFavoriteRecipes(user_id){
-    const recipes_id = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
+    const recipes_id = await DButils.execQuery(`select recipe_id from favoriterecipes where user_id='${user_id}'`);
     return recipes_id;
 }
 
 async function removeFromFavorites(username, recipe_id) {
-    await DButils.execQuery(`DELETE FROM FavoriteRecipes WHERE username='${username}' AND recipe_id=${recipe_id}`);
+    await DButils.execQuery(`DELETE FROM favoriterecipes WHERE username='${username}' AND recipe_id=${recipe_id}`);
 }
 
 exports.markAsFavorite = markAsFavorite;
