9e8724f09563c41a856489ed662ac1a8649d0c31
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2024-07-09 20:00:37 +0300

    Add search option and save last search

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const DButils = require("./utils/DButils");
 const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
@@ -17,15 +18,32 @@ router.get("/:recipeId", async (req, res, next) => {
   }
 });
 
-// search recipes
+
+/**
+ * Applay search - by query and insert to DB (for last search)
+ */
 router.post("/search", async (req, res, next) => {
-  console.log(req.body);
   try {
+    const username = req.session.username;
+    console.log("Hi I'm POST")
     let recipes = await recipes_utils.searchRecipe(req.body);
+    const searchQuery = req.query.query;
+    const results = await DButils.execQuery(`SELECT * FROM lastsearch WHERE username ='${username}'`);
+    if (results.length > 0) {
+        // User exists, so update the search query
+        console.log("Update " + searchQuery)
+        await DButils.execQuery(`UPDATE lastsearch SET searchQuery='${searchQuery}' WHERE username='${username}'`);
+
+    } else {
+        // User does not exist, so insert a new record
+        console.log("Insert " + searchQuery)
+        await DButils.execQuery(`INSERT INTO lastsearch VALUES ('${searchQuery}', '${username}')`);
+    }
     res.send(recipes);
   } catch (error) {
     next(error);
   }
 });
+
 
 module.exports = router;
